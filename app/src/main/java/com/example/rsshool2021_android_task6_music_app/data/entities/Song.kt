b35c93b021d5c09c1790e620b4fb7c1673472620
BlackHandMinsk <3058079@gmail.com>
2021-10-17 18:47:28 +0300

    getting songs from database

---
+++ app/src/main/java/com/example/rsshool2021_android_task6_music_app/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.example.rsshool2021_android_task6_music_app.data.entities
+
+data class Song (
+    val mediaId:String = "",
+    val tittle:String = "",
+    val artist:String = "",
+    val bitmapUri:String = "",
+    val trackUri:String = "",
+)
---
+++ app/src/main/java/com/example/rsshool2021_android_task6_music_app/other/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.rsshool2021_android_task6_music_app.other
+
+object Constants{
+
+    const val SONG_COLLECTION = "songs"
+}
