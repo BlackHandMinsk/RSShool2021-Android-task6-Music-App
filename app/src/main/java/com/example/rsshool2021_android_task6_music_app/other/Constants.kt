05b69d1c5a456e86b97acd6598c7ab312b06d2ae
BlackHandMinsk <3058079@gmail.com>
2021-10-21 16:34:47 +0300

    finishing the music service

--- app/src/main/java/com/example/rsshool2021_android_task6_music_app/other/Constants.kt
+++ app/src/main/java/com/example/rsshool2021_android_task6_music_app/other/Constants.kt
@@ -3,6 +3,9 @@ package com.example.rsshool2021_android_task6_music_app.other
 object Constants{
 
     const val SONG_COLLECTION = "songs"
+
+    const val MEDIA_ROOT_ID = "root_id"
+
     const val NOTIFICATION_ID = 1
     const val NOTIFICATION_CHANNEL_ID = "music"
 }
