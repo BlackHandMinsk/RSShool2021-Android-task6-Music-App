0ebc9257f6f16342e67aaf95954889ad25d2b0b5
BlackHandMinsk <3058079@gmail.com>
2021-10-21 17:08:09 +0300

    event and resource

---
+++ app/src/main/java/com/example/rsshool2021_android_task6_music_app/other/Event.kt
@@ -0,0 +1,20 @@
+package com.example.rsshool2021_android_task6_music_app.other
+
+open class Event<out T>(private val data:T) {
+
+
+    var hasBeenHandled = false
+                   private set
+
+
+    fun getContentIfNotHandled():T?{
+        return if (hasBeenHandled){
+            null
+        }else{
+            hasBeenHandled = true
+            data
+        }
+    }
+
+    fun picContent() = data
+}
