1823d8409f6bef8a45141ac73b44669e0bc863e3
BlackHandMinsk <3058079@gmail.com>
2021-10-22 22:19:52 +0300

    main view model

--- app/src/main/java/com/example/rsshool2021_android_task6_music_app/di/AppModule.kt
+++ app/src/main/java/com/example/rsshool2021_android_task6_music_app/di/AppModule.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.example.rsshool2021_android_task6_music_app.R
+import com.example.rsshool2021_android_task6_music_app.exoplayer.MusicServiceConnection
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,6 +19,12 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context
+    ) = MusicServiceConnection(context)
 
     @Singleton
     @Provides
